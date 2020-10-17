48f72ffa81fbf8323f243cfdf36b83d9dba6c295
vasyas <vasyl@stashuk.com>
2020-10-17 20:19:13 +0300

    wip

--- tests/simple.ts
+++ tests/simple.ts
@@ -1,5 +1,5 @@
-import { assert } from "chai"
-import {LiveQuery} from "../src"
+import {assert} from "chai"
+import {LiveQuery} from "../src/LiveQuery"
 
 describe("simple", () => {
   let testData = []
