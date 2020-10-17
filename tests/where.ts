327f16a628bbaab9b3f0b47d0d1d8c2de09e3b5a
vasyas <vasyl@stashuk.com>
2020-10-17 21:41:00 +0300

    where test

---
+++ tests/where.ts
@@ -0,0 +1,38 @@
+import {assert} from "chai"
+import {LiveQuery} from "../src/LiveQuery"
+import {adelay, Context, sql} from "./db"
+import {mockSession, testData} from "./mockSession"
+
+describe("where track", () => {
+  it("single field", async () => {
+    const liveQuery = new LiveQuery(async (_, ctx: Context) => {
+      await ctx.sql("select * from Test where id = 1")
+    })
+
+    await liveQuery.subscribeSession(mockSession, {})
+    assert.equal(1, testData.length)
+
+    await sql("insert into Test(id) values(1)")
+    await adelay(10)
+    assert.equal(2, testData.length)
+
+    // this one is ignored
+    await sql("insert into Test(id) values(2)")
+    await adelay(10)
+    assert.equal(2, testData.length)
+  })
+
+  // operators: like, in, gt, lt
+  // combinators: and, or
+  // page (limit, offset)
+
+  // only certaint fields
+
+  // functinos: lower etc
+
+  // inner joins
+
+  // operators with json
+
+  // groups?
+})
