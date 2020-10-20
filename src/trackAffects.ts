c8899937358f8e79fd5699679d393fe3cf62ab7d
vasyas <vasyl@stashuk.com>
2020-10-20 21:45:15 +0300

    where: wip

--- src/trackAffects.ts
+++ src/trackAffects.ts
@@ -2,12 +2,57 @@ import {Row} from "binlog-triggers-mysql"
 
 export type TrackAffects = (row: Row) => boolean
 
-export function createTrackAffects(astWhere): TrackAffects {
-  if (!astWhere) return always
+export function createTrackAffects(where): TrackAffects {
+  if (!where) return always
 
-  console.log(astWhere)
+  const impl = expr(where)
 
-  return always
+  return (row) => {
+    try {
+      return impl(row)
+    } catch (e) {
+      console.log(e)
+
+      // affects by default
+      return true
+    }
+  }
+}
+
+// parsing based on https://github.com/taozhi8833998/node-sql-parser/blob/master/ast/postgresql.ts
+
+function expr(node) {
+  switch (node.type) {
+    case "binary_expr":
+      return binary_expr(node)
+    case "number":
+      return () => node.value
+    case "column_ref":
+      return column_ref(node)
+    default:
+      return always
+  }
+}
+
+function column_ref(node) {
+  return (row) => {
+    return row[node.column]
+  }
+}
+
+function binary_expr(node) {
+  const left = expr(node.left)
+  const right = expr(node.right)
+
+  return (row) => {
+    const leftValue = left(row)
+    const rightValue = right(row)
+
+    if (node.operator == "=") return leftValue == rightValue
+
+    // by default always affects
+    return true
+  }
 }
 
 const always = () => true
