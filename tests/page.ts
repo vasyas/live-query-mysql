a7dc7929654d2855643bd8df994d0397e133d36f
vasyas <vasyl@stashuk.com>
2021-01-28 12:23:11 +0200

    more tests

---
+++ tests/page.ts
@@ -0,0 +1,24 @@
+import {assert} from "chai"
+import {LiveQuery} from "../src/LiveQuery"
+import {adelay, Context, sql} from "./db"
+import {mockSession, testData} from "./mockSession"
+
+describe("limit/offset track", () => {
+  it("ignored", async () => {
+    const liveQuery = new LiveQuery((_, ctx: Context) =>
+      ctx.sql("select * from Test where id = 1 limit 1 offset 0")
+    )
+
+    await liveQuery.subscribeSession(mockSession, {}, "1", {})
+    assert.equal(1, testData.length)
+
+    await sql("insert into Test(id) values(1)")
+    await adelay(10)
+    assert.equal(2, testData.length)
+
+    // this one is ignored
+    await sql("insert into Test(id) values(2)")
+    await adelay(10)
+    assert.equal(2, testData.length)
+  })
+})
